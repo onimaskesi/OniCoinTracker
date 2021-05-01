250e2ad0f09d96c2d63a1b432f2c0832c0d33050
onimaskesi <53870740+onimaskesi@users.noreply.github.com>
2021-05-01 18:19:39 +0300

    push service

---
+++ app/src/main/java/com/onimaskesi/onicointracker/OniCoinTrackerApp.kt
@@ -0,0 +1,31 @@
+package com.onimaskesi.onicointracker
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+
+class OniCoinTrackerApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannels()
+    }
+
+    fun createNotificationChannels(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            val channel1 = NotificationChannel(
+                    "channel_1",
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_HIGH)
+
+            channel1.description = "Daily notification"
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel1)
+
+        }
+    }
+
+}
--- app/src/main/java/com/onimaskesi/onicointracker/view/MainActivity.kt
+++ app/src/main/java/com/onimaskesi/onicointracker/view/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.onimaskesi.onicointracker.view
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.vectordrawable.graphics.drawable.ArgbEvaluator
+import com.huawei.hms.push.HmsMessageService
+import com.huawei.hms.push.HmsMessaging
 import com.onimaskesi.onicointracker.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.coin_recycler_raw.view.*
@@ -19,5 +28,8 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(this, R.id.fragment)
         bottomNavigation.setupWithNavController(navController)
+
+        val hmsMessaging = HmsMessaging.getInstance(this)
+        hmsMessaging.isAutoInitEnabled = true
     }
 }
